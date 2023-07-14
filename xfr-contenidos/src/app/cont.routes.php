f62df679fcfa48f861f33942b80628f0b823ada4
marce00100 <37050021+marce00100@users.noreply.github.com>
2023-07-13 23:39:23 -0400

    julio13

--- xfr-contenidos/src/app/cont.routes.php
+++ xfr-contenidos/src/app/cont.routes.php
@@ -4,22 +4,21 @@ $contenidoPrefijRuta = "cont/v1/";
 Route::post($contenidoPrefijRuta, 'get-contents'          , 'Cont@getContents');
 Route::post($contenidoPrefijRuta, 'get-content'           , 'Cont@getContent');
 Route::post($contenidoPrefijRuta, 'save-content-upload'   , 'Cont@saveContenidoUpload');
-// Route::post($contenidoPrefijRuta, 'file-upload'    , 'Cont@fileUpload');
 
-Route::post($contenidoPrefijRuta, 'contents-bj'    , 'ContBJ@contentsBJ');
-Route::post($contenidoPrefijRuta, 'get-content-bj' , 'ContBJ@getContentBJ');
+Route::post($contenidoPrefijRuta, 'contents-bj'           , 'ContBJ@contentsBJ');
+Route::post($contenidoPrefijRuta, 'get-content-bj'        , 'ContBJ@getContentBJ');
 
-Route::post($contenidoPrefijRuta, 'contents-normas'    , 'CreaNormas@contentsNormas');
-Route::post($contenidoPrefijRuta, 'get-bj' ,        'CreaNormas@getContentBJ');
-Route::post($contenidoPrefijRuta, 'save-bj-upload' ,     'CreaNormas@saveBJUpload');
+Route::post($contenidoPrefijRuta, 'contents-normas'       , 'CreaNormas@contentsNormas');
+Route::post($contenidoPrefijRuta, 'get-bj'                , 'CreaNormas@getContentBJ');
+Route::post($contenidoPrefijRuta, 'save-bj-upload'        , 'CreaNormas@saveBJUpload');
 
 //TODO ver quitar o mantener
-Route::post($contenidoPrefijRuta, 'contents-sentencias'     , 'Cont@getContents');
-Route::post($contenidoPrefijRuta, 'get-content-sentencia'   , 'ContBJ@getContentSentencia');
+Route::post($contenidoPrefijRuta, 'contents-sentencias'   , 'Cont@getContents');
+Route::post($contenidoPrefijRuta, 'get-content-sentencia' , 'ContBJ@getContentSentencia');
 
 
-Route::post($contenidoPrefijRuta, 'consulta'       , 'ContBJ@consultaQuery');
-Route::post($contenidoPrefijRuta, 'get-paises'       , 'CreaNormas@paises');
+Route::post($contenidoPrefijRuta, 'consulta'              , 'ContBJ@consultaQuery');
+Route::post($contenidoPrefijRuta, 'get-paises'            , 'CreaNormas@paises');
 Route::post($contenidoPrefijRuta, 'get-registros_uniqos'  , 'ContBJ@distinct');
 
 
